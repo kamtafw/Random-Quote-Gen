0540516785da1bf9e10981be2eb6e4d2e0bfef21
Salvation Tiva <tivasalvation@gmail.com>
2025-07-25 23:10:57 +0100

    update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -99,25 +99,25 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 # DATABASES = {"default": dj_database_url.config(default=env("DATABASE_URL"))}
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": env("PGDATABASE"),
-#         "USER": env("PGUSER"),
-#         "PASSWORD": env("PGPASSWORD"),
-#         "HOST": env("PGHOST"),
-#         "PORT": env("PGPORT"),
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("PGDATABASE"),
+        "USER": env("PGUSER"),
+        "PASSWORD": env("PGPASSWORD"),
+        "HOST": env("PGHOST"),
+        "PORT": env("PGPORT"),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
